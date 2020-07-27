9bca8d93314fec504179b703a3eb21525858b7cc
cazsol <theoneas1@gmail.com>
2020-07-27 00:58:10 -0500

    1st review of chapter one and first 2 exerises ompleted

--- pathfixer.py
+++ pathfixer.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.insert(0,'/home/k/GIT/ThinkBayes2/')
+sys.path.insert(0,'/home/k/GIT/ThinkBayes2/thinkbayes2/')
+sys.path.insert(1,'/home/k/GIT/ThinkBayes2/thinkplot/')
 print('local module ThinkBayes2 was added to Python path')
 for i in sys.path:
     print(i)
